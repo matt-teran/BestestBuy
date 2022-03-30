fae8120f1838653b390074acd13d5a99b4f3fd9d
Vince <vincent.tran.ece@gmail.com>
2022-03-30 16:36:02 -0500

    testing merge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
-        return <div>Hello World</div>
+        return (
+            <div>Hello World</div>
+            <div>Hello Tean Saturn</div>
+        )
+
     }
 };
 
