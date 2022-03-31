727bc3aea9bacfe1dfd7e2a3f0b6b84d823602d7
Kun Chen <justinchen9387@gmail.com>
2022-03-31 15:13:18 -0700

    connect to API

---
+++ src/components/overview/Product infomation/ProductCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductCategory() {
+  return (
+    <div>
+      <h4> Category </h4>
+    </div>
+  );
+}
+
+export default ProductCategory;
--- src/components/overview/ProductInformation.jsx
+++ src/components/overview/ProductInformation.jsx
@@ -1,4 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+import config from './config';
+
+const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/';
+
+const getData = () => {
+  axios.get(
+    `${url}products?page=1&count=5`,
+    {
+      headers: {
+        Authorization: config.TOKEN,
+      },
+    },
+  )
+    .then((result) => console.log(result))
+    .catch();
+};
+
+getData();
 
 class ProductInformation extends React.Component {
   constructor(props) {
