01a24d487f775514f964cd7d2c017cf67d59318a
Nick Sooter <nick.sooter@gmail.com>
2022-04-03 17:18:54 -0600

    started search feature, working on Q&A layout

---
+++ src/components/Q&A/Question.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Question() {
+  return (
+    <div>
+      <div className='question'>
+        <h4>Q: Is this a question?</h4>
+      </div>
+      <div className='answer'>
+        <h4>A: This is an answer!</h4>
+      </div>
+    </div>
+  );
+}
+/*
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+
+    render() {
+      return (
+        <div>
+          <h3>Questions & Answers</h3>
+          <div className='question'>
+            <h4>Q: Is this a question?</h4>
+          </div>
+          <div className='answer'>
+            <h4>A: This is an answer!</h4>
+          </div>
+        </div>)
+    }
+  }
+*/
+export default Question;
