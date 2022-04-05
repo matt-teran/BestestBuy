017e0899b3c1dec02edf7ccdd9cbd9ccb45c5c6d
Matt Teran <matthew.a.teran@gmail.com>
2022-04-05 15:24:45 -0500

    adds documentation to Backdrop and Modal components

--- src/components/ui/Modal/Backdrop.jsx
+++ src/components/ui/Modal/Backdrop.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import propTypes from 'prop-types';
 import './Modal.scss';
 
+/**
+ * This component should be rendered next to <Modal></Modal>
+ * @param {boolean} showModal
+ * This value determines whether the backdrop is rendered.
+ * Handle this value in the state of the component where this backdrop is being rendered.
+ * @param {function} clickHandler
+ * This clickHandler is intended to reference a function that sets the showModal state to false.
+ * This will close the modal if the backdrop is clicked.
+ * @returns React Component
+ */
+
 function Backdrop({ showModal, clickHandler }) {
   const newLocal = 'modal-overlay closed';
   return (
--- src/components/ui/Modal/Modal.jsx
+++ src/components/ui/Modal/Modal.jsx
@@ -2,6 +2,16 @@ import propTypes from 'prop-types';
 import React from 'react';
 import './Modal.scss';
 
+/**
+ * This component should be rendered next to <Backdrop />
+ * @param {boolean} showmodal
+ * This value determines whether the modal is rendered.
+ * Handle this value in the state of the component where this modal is being rendered.
+ * @param {node} children
+ * Children is defined as whatever is wrapped by this modal component.
+ * example: <Modal>[children]</Modal>
+ * @returns React Component
+ */
 function Modal({ showModal, children }) {
   return (
     <div className={showModal ? 'modal' : 'modal closed'}>
