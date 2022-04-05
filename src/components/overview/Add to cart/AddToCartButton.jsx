45ebf34e16b4adc764e3a8232095a38af7ebaa6c
Kun Chen <justinchen9387@gmail.com>
2022-04-05 14:14:29 -0700

    create add to cart button, but need add function to the button later

--- src/components/overview/Add to cart/AddToCartButton.jsx
+++ src/components/overview/Add to cart/AddToCartButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../ProductOverview.scss';
 
 function AddtoCartButton() {
   return (
     <div>
-      <p>AddToCartButton</p>
+      <button className="add_to_cart_button" type="submit">Add to bag</button>
     </div>
   );
 }
