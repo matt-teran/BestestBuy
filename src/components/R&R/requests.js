a823daa4bb18a58cfb94a210ff2da9028b7a3f6e
Vince <vincent.tran.ece@gmail.com>
2022-04-06 12:02:39 -0500

    udpated to use common template for API key

--- src/components/R&R/requests.js
+++ src/components/R&R/requests.js
@@ -1,15 +1,13 @@
 /* eslint-disable camelcase */
 const axios = require('axios');
-const { Api_Key } = require('../../config');
-
-const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/';
+const { API_KEY, url } = require('../../config');
 
 const getReviews = function (productId, page = 1, sort = 'relevant') {
   // console.log('productId: ', productId, ' page: ', page, ' sort: ', sort); // for testing
 
-  return axios.get(`${url}reviews?product_id=${productId}&page=${page}&sort=${sort}`, {
+  return axios.get(`${url}/reviews?product_id=${productId}&page=${page}&sort=${sort}`, {
     headers: {
-      Authorization: Api_Key,
+      Authorization: API_KEY,
     },
   });
 };
