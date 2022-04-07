7278d54e2e69306fcb3eabe7bcb75030c0926aa6
Vince <vincent.tran.ece@gmail.com>
2022-04-07 11:38:24 -0500

    saves progress for R&R

--- src/components/R&R/ReviewTile.jsx
+++ src/components/R&R/ReviewTile.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
-import Rating from '../ui/Rating/Rating';
 import moment from 'moment';
+import Rating from '../ui/Rating/Rating';
 
 function Tile({ review }) {
   const datePosted = review.date.slice(0, 10);
@@ -14,14 +15,16 @@ function Tile({ review }) {
       <h3>{review.summary}</h3>
       <p>{review.body}</p>
       <div className="review-Tile-Images">
-        {review.photos.map((imgObj, index) => {
-          return <img className="review-Image" key={`image${imgObj.id}`} src={imgObj.url} alt="https://images.unsplash.com/photo-1490127252417-7c393f993ee4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"/>
-        })}
+        {review.photos.map((imgObj, index) => <img className="review-Image" key={`image${imgObj.id}`} src={imgObj.url} alt="https://images.unsplash.com/photo-1490127252417-7c393f993ee4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" />)}
       </div>
       <div className="helpfulAndReport">
-      <button type="button">Helpful({review.helpfulness})</button>
-      <button type="button">Report</button>
-      <button type="button">Comment</button>
+        <button type="button">
+          Helpful(
+          {review.helpfulness}
+          )
+        </button>
+        <button type="button">Report</button>
+        <button type="button">Comment</button>
       </div>
     </section>
   ) : (<></>);
