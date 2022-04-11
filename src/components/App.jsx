b0e38b72e749b7a6d86f6c229c772afd69e620b0
Matt Teran <matthew.a.teran@gmail.com>
2022-04-11 12:52:05 -0500

    adds a button to display the relatedProducts widget

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,15 @@ import RatingsAndReviews from './R&R/RatingsAndReviews';
 function App() {
   const { paramId } = useParams();
   const [productId, setProductId] = useState(paramId);
+  const [showRelated, setShowRelated] = useState(false);
   useEffect(() => {
     setProductId(paramId);
+    setShowRelated(false);
   }, [paramId]);
   return (
     <div className="app" key={productId}>
       <div><ProductOverview id={productId} /></div>
-      <RelatedProducts id={productId} />
+      {showRelated ? <RelatedProducts id={productId} /> : <button onClick={() => setShowRelated(true)} type="button">Show Related Products</button>}
       <div><QuestionsList id={productId} /></div>
       <div id="ratings-and-reviews"><RatingsAndReviews id={productId} /></div>
     </div>
