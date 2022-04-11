3e9490223c8043a49f008695251c73837a896d9e
Matt Teran <matthew.a.teran@gmail.com>
2022-04-11 17:11:03 -0500

    adds styling

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function Search() {
   return (
     <div className="header-search">
       <input />
-      <div className="search-icon" />
+      <FontAwesomeIcon icon={faMagnifyingGlass} />
     </div>
   );
 }
