60ad34bcd2f8d0720afa41677494d28cf9dffed7
Matt Teran <matthew.a.teran@gmail.com>
2022-04-12 13:44:46 -0500

    adds higher order component that gathers click data and posts it to API

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
-import Header from './header/Header';
 
 function AppRouter() {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/:paramId" element={<App />} />
-        <Route path="*" element={<Navigate replace to="/66642" />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/:paramId" element={<App />} />
+      <Route path="*" element={<Navigate replace to="/66642" />} />
+    </Routes>
   );
 }
 
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -3,13 +3,10 @@ import Search from './Search';
 
 function Navbar() {
   return (
-    <>
-      <div className="navbar">
-        <h1>BestestBuy</h1>
-        <Search />
-      </div>
-      <div />
-    </>
+    <div className="navbar">
+      <h1>BestestBuy</h1>
+      <Search />
+    </div>
   );
 }
 
