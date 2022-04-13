d63c6f5f95784e4922c60292a0fdaa264a7a77c8
Matt Teran <matthew.a.teran@gmail.com>
2022-04-13 14:07:04 -0500

    refactors code to use hooks

---
+++ src/components/relatedProducts/ProductsList.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+import ProductCard from './productCard/ProductCard';
+import Btn from '../ui/Btn/Btn';
+// import AddToOutfit from './productCard/AddToOutfit';
+
+function ProductsList({
+  title, cards, outfit, clickHandler, children
+}) {
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(5);
+
+  const scrollHandler = (direction) => {
+    if (direction === 'left') {
+      setStart(start - 1);
+      setEnd(end - 1);
+    } else {
+      setStart(start + 1);
+      setEnd(end + 1);
+    }
+  };
+  const cardComponents = cards.map((card) => (
+    <ProductCard
+      key={card.id}
+      avgRating={card.avgRating}
+      price={card.default_price}
+      name={card.name}
+      category={card.category}
+      image={card.image || ''}
+      clickHandler={clickHandler}
+      id={card.id}
+      outfit={!!children}
+    />
+  ));
+  return (
+    <>
+      <h2 className="list-title">{ title }</h2>
+      <div className="products-list">
+        {start > 0 ? <Btn char="◀" className="scroll-left" clickHandler={() => scrollHandler('left')} /> : null}
+        {end < cards.length ? <Btn char="▶" className="scroll-right" clickHandler={() => scrollHandler('right')} /> : null}
+        {children}
+        {cardComponents.slice(start, end)}
+      </div>
+    </>
+  );
+}
+
+ProductsList.defaultProps = {
+  cards: [],
+  outfit: false,
+};
+
+ProductsList.propTypes = {
+  title: propTypes.string.isRequired,
+  cards: propTypes.arrayOf(propTypes.shape({
+    id: propTypes.number.isRequired,
+    default_price: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+    category: propTypes.string.isRequired,
+  })),
+  outfit: propTypes.bool,
+  clickHandler: propTypes.func.isRequired,
+};
+
+export default ProductsList;
--- src/components/relatedProducts/RelatedProducts.jsx
+++ src/components/relatedProducts/RelatedProducts.jsx
@@ -1,47 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import propTypes from 'prop-types';
 import Cookies from 'js-cookie';
 
 import { url, headers } from '../../config';
-import RelatedProductsList from './RelatedProductsList';
+// import RelatedProductsList from './RelatedProductsList';
+import ProductsList from './ProductsList';
 import Modal from '../ui/Modal/Modal';
 import Backdrop from '../ui/Modal/Backdrop';
 import Comparison from './comparisonModal/comparison/Comparison';
 
 import './RelatedProducts.scss';
+import AddToOutfit from './productCard/AddToOutfit';
 
-class RelatedProducts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      relatedCards: [],
-      outfitCards: [],
-      showModal: false,
-      currentProductInfo: null,
-      comparedProductInfo: null,
-    };
-    this.openModal = this.openModal.bind(this);
-    this.addToOutfit = this.addToOutfit.bind(this);
-    this.removeFromOutfit = this.removeFromOutfit.bind(this);
-  }
+function RelatedProducts({ id }) {
+  const [relatedCards, setRelatedCards] = useState([]);
+  const [outfitCards, setOutfitCards] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [currentProductInfo, setCurrentProductInfo] = useState(null);
+  const [comparedProductInfo, setComparedProductInfo] = useState(null);
 
-  componentDidMount() {
-    this.getRelatedProducts();
-    this.getCurrentProductInfo();
-    this.setOutfitCookieAsState();
-  }
-
-  getCurrentProductInfo() {
-    const { id } = this.props;
+  const getCurrentProductInfo = () => {
     axios.get(`${url}/products/${id}`, headers)
-      .then(({ data }) => this.setState({ currentProductInfo: data }))
+      .then(({ data }) => setCurrentProductInfo(data))
       .catch((err) => { console.log(err); });
-  }
+  };
 
-  getRelatedProducts() {
-    const { id } = this.props;
-
+  const getRelatedProducts = () => {
     function getAvgRating(ratings) {
       const ratingsArray = Object.keys(ratings);
       if (ratingsArray.length === 0) {
@@ -82,17 +67,15 @@ class RelatedProducts extends React.Component {
       })
       .then(() => Promise.all(cards.map((card) => axios.get(`${url}/reviews/meta?product_id=${card.id}`, headers))))
       .then((res) => {
-        this.setState({
-          relatedCards: cards.map((card, i) => ({
-            ...card,
-            avgRating: getAvgRating(res[i].data.ratings),
-          })),
-        });
+        setRelatedCards(cards.map((card, i) => ({
+          ...card,
+          avgRating: getAvgRating(res[i].data.ratings),
+        })));
       })
       .catch((err) => { console.log(err); });
-  }
+  };
 
-  setOutfitCookieAsState() {
+  const setOutfitCookieAsState = () => {
     function getAvgRating(ratings) {
       const ratingsArray = Object.keys(ratings);
       if (ratingsArray.length === 0) {
@@ -108,11 +91,11 @@ class RelatedProducts extends React.Component {
     }
     if (!Object.keys(Cookies.get()).includes('outfit')) {
       Cookies.set('outfit', JSON.stringify([]));
-      this.setState({ outfitCards: JSON.parse(Cookies.get('outfit')) });
+      setOutfitCards(JSON.parse(Cookies.get('outfit')))
     } else {
       const outfitIds = JSON.parse(Cookies.get('outfit'));
       let cards;
-      Promise.all(outfitIds.map((id) => axios.get(`${url}/products/${id}`, headers)))
+      Promise.all(outfitIds.map((_id) => axios.get(`${url}/products/${_id}`, headers)))
         .then((data) => {
           cards = data.map((res) => {
             const card = res.data;
@@ -133,64 +116,67 @@ class RelatedProducts extends React.Component {
         })
         .then(() => Promise.all(cards.map((card) => axios.get(`${url}/reviews/meta?product_id=${card.id}`, headers))))
         .then((res) => {
-          this.setState({
-            outfitCards: cards.map((card, i) => ({
-              ...card,
-              avgRating: getAvgRating(res[i].data.ratings),
-            })),
-          });
+          setOutfitCards(cards.map((card, i) => ({
+            ...card,
+            avgRating: getAvgRating(res[i].data.ratings),
+          })));
         })
         .catch((err) => { console.log(err); });
     }
-  }
+  };
 
-  openModal(comparedId) {
+  const openModal = (comparedId) => {
     axios.get(`${url}/products/${comparedId}`, headers)
-      .then(({ data }) => { this.setState({ comparedProductInfo: data, showModal: true }); })
+      .then(({ data }) => {
+        setComparedProductInfo(data);
+        setShowModal(true);
+      })
       .catch((err) => { console.log(err); });
-  }
+  };
 
-  addToOutfit() {
-    const { id } = this.props;
+  const addToOutfit = () => {
     const updatedOutfitCards = [...new Set([id, ...JSON.parse(Cookies.get('outfit'))])];
     Cookies.set('outfit', JSON.stringify(updatedOutfitCards));
-    this.setOutfitCookieAsState();
-  }
+    setOutfitCookieAsState();
+  };
 
-  removeFromOutfit(id) {
-    const updatedOutfitCards = JSON.parse(Cookies.get('outfit')).filter((outfitId) => outfitId !== id.toString());
+  const removeFromOutfit = (_id) => {
+    const updatedOutfitCards = JSON.parse(Cookies.get('outfit')).filter((outfitId) => outfitId !== _id.toString());
     Cookies.set('outfit', JSON.stringify(updatedOutfitCards));
-    this.setOutfitCookieAsState();
-  }
+    setOutfitCookieAsState();
+  };
 
-  render() {
-    const {
-      relatedCards, outfitCards, showModal, currentProductInfo, comparedProductInfo,
-    } = this.state;
+  useEffect(() => {
+    console.log('hello from effect');
+    getRelatedProducts();
+    getCurrentProductInfo();
+    setOutfitCookieAsState();
+  }, []);
 
-    if (relatedCards.length === 0) {
-      return <div className="related-products-loading">Loading...</div>;
-    }
-    return (
-      <>
-        <Modal showModal={showModal}>
-          {currentProductInfo && comparedProductInfo
-            ? (
-              <Comparison
-                currentProduct={currentProductInfo.features}
-                comparedProduct={comparedProductInfo.features}
-              />
-            )
-            : null}
-        </Modal>
-        <Backdrop showModal={showModal} clickHandler={() => this.setState({ showModal: false })} />
-        <div className="related-products">
-          <RelatedProductsList title="Related Products" relatedCards={relatedCards} clickHandler={this.openModal} />
-          <RelatedProductsList className="outfit-list" title="Outfit List" outfit relatedCards={outfitCards} clickHandler={this.removeFromOutfit} addToOutfit={this.addToOutfit} />
-        </div>
-      </>
-    );
+  if (relatedCards.length === 0) {
+    return <div className="related-products-loading">Loading...</div>;
   }
+  return (
+    <>
+      <Modal showModal={showModal}>
+        {currentProductInfo && comparedProductInfo
+          ? (
+            <Comparison
+              currentProduct={currentProductInfo.features}
+              comparedProduct={comparedProductInfo.features}
+            />
+          )
+          : null}
+      </Modal>
+      <Backdrop showModal={showModal} clickHandler={() => setShowModal(false)} />
+      <div className="related-products">
+        <ProductsList title="Related Products" cards={relatedCards} clickHandler={openModal} />
+        <ProductsList className="outfit-list" title="Outfit List" cards={outfitCards} clickHandler={removeFromOutfit}>
+          <AddToOutfit addToOutfit={addToOutfit} />
+        </ProductsList>
+      </div>
+    </>
+  );
 }
 
 RelatedProducts.propTypes = {
