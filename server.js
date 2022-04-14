055fb40aa4f14bdf21a721f43ae7bba4b477a320
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-04-13 20:58:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 8000;
-app.use(express.static(path.join(__dirname, 'client', 'public')));
 
+// app.use(express.static(path.join(__dirname, 'client', 'public')));
+app.get('/', (req, res) => {
+    res.send('hello from ec2 server');
+})
 app.listen(PORT, () => {
     console.log('Listening on ' + PORT);
 })
