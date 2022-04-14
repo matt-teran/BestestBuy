ccc3a18c8f64b5694b245999e6bf20b65cc8a1d3
Matt Teran <matthew.a.teran@gmail.com>
2022-04-13 23:57:51 -0500

    imports api key from environment variables

---
+++ client/src/config.js
@@ -0,0 +1,7 @@
+export const headers = {
+  headers: {
+    Authorization: process.env.API_KEY,
+  },
+};
+
+export const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc';
---
+++ client/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
