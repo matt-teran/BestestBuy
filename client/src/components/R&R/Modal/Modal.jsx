aa1f6cb48b826d266b32d76636b6a1b0458707ad
Vince <vincent.tran.ece@gmail.com>
2022-04-14 20:40:44 -0500

    saves progress. Adds report button

---
+++ client/src/components/R&R/Modal/Modal.jsx
@@ -0,0 +1,31 @@
+import propTypes from 'prop-types';
+import React from 'react';
+import './Modal.scss';
+
+/**
+ * This component should be rendered next to <Backdrop />
+ * @param {boolean} showmodal
+ * This value determines whether the modal is rendered.
+ * Handle this value in the state of the component where this modal is being rendered.
+ * @param {node} children
+ * Children is defined as whatever is wrapped by this modal component.
+ * example: <Modal>[children]</Modal>
+ * @returns React Component
+ */
+function Modal({ showModal, children }) {
+  return (
+    <div className={showModal ? 'ratings-reviews-modal' : 'modal closed'}>
+      { children }
+    </div>
+  );
+}
+Modal.defaultProps = {
+  children: null,
+};
+
+Modal.propTypes = {
+  showModal: propTypes.bool.isRequired,
+  children: propTypes.node,
+};
+
+export default Modal;
