f3b1025a1e2f3bc8a770ba73030fd90d9c4493c9
Matt Teran <matthew.a.teran@gmail.com>
2022-05-02 21:47:18 -0500

    cleans up folder structure. refactors webpack to live in the root directory. creates new npm scripts so that nodemon and webpack will watch for changes at the same time

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
-import AppRouter from './components/AppRouter';
+import AppRouter from './components/AppRouter.jsx';
 
 reactDOM.render(<HashRouter><AppRouter /></HashRouter>, document.getElementById('root'));
--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-var expressStaticGzip = require("express-static-gzip");
-var compression = require('compression');
+const expressStaticGzip = require('express-static-gzip');
+const compression = require('compression');
 
-const { API_KEY } = require('./config.js');
+const { API_KEY } = require('./config');
 
 const app = express();
 
 app.use(compression());
-app.use("/", expressStaticGzip("BestestBuy", { ensureGzipedFiles: true }));
-app.use(express.static(path.join(__dirname, 'client', 'public')));
+app.use('/', expressStaticGzip('BestestBuy', { ensureGzipedFiles: true }));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 app.use(express.json());
 
-//receive all requests
+// receive all requests
 app.use('*', (req, res) => {
-    const method = req.method;
-    const url = `${`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc`}${req.originalUrl}`;
-    const headers = { Authorization: API_KEY };
-    const data = req.body;
-    //forward request w/ config to API
-    axios({ method, url, headers, data })
-        //send received data to client
-        .then(({ data }) => { res.send(data); })
-        .catch((err) => { res.send(err) });
-})
+  const { method } = req;
+  const url = `${'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc'}${req.originalUrl}`;
+  const headers = { Authorization: API_KEY };
+  const { body } = req;
+  // forward request w/ config to API
+  axios({
+    method, url, headers, data: body,
+  })
+  // send received data to client
+    .then(({ data }) => { res.send(data); })
+    .catch((err) => { res.send(err); });
+});
 
 app.listen(8080, () => {
-    console.log('Listening on port 8080');
+  console.log('Listening on port 8080');
 });
+
+// "react-image-magnifiers": "^1.4.0",
